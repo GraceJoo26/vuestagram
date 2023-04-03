08afb5f08b2d96f486d6e7cd2c3ca98552d29faa
gracejoo26 <gracejoo26@gmail.com>
2023-04-03 21:27:03 +0900

    props inner

---
+++ src/components/Container.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <Post :name="name" :date="date" :content="content" :likes="likes" :ref="ref"/>
+  <Post :name="name" :date="date" :content="content" :likes="likes" :ref="ref"/>
+  <Post :name="name" :date="date" :content="content" :likes="likes" :ref="ref"/>
+</div>
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+ name:'',
+ components:{
+  Post,
+ },
+ props:{
+  name: String,
+  likes: Number,
+  date: Number,
+  content: String,
+  filter: String,
+ }
+}
+</script>
+
+<style>
+
+</style>
